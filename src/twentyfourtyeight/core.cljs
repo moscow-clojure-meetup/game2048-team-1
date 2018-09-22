1d6646bdf3cc2f8c5b0615e1e442e6c8c65f6ddb
Nikita Prokopov <prokopov@gmail.com>
2018-09-22 17:02:03 +0300

    sample model

--- src/twentyfourtyeight/core.cljs
+++ src/twentyfourtyeight/core.cljs
@@ -2,5 +2,13 @@
 
 (enable-console-print!)
 
+;; {[0 0] => 2048,
+;;  [0 1] => nil, ...}
+(def model
+  (into {}
+    (for [x (range 0 4)
+          y (range 0 4)]
+      [[x y] nil])))
+
 (defn ^:export refresh []
   (println "Hello"))
