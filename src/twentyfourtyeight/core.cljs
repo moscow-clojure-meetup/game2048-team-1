667b958a7d8fe276daed39d093a67ef9c94763a0
Nikita Prokopov <prokopov@gmail.com>
2018-09-22 17:03:24 +0300

    added score to model

--- src/twentyfourtyeight/core.cljs
+++ src/twentyfourtyeight/core.cljs
@@ -3,12 +3,16 @@
 (enable-console-print!)
 
 ;; {[0 0] => 2048,
-;;  [0 1] => nil, ...}
+;;  [0 1] => nil,
+;;  ...,
+;;  :score => 1000}
 (def model
-  (into {}
-    (for [x (range 0 4)
-          y (range 0 4)]
-      [[x y] nil])))
+  (assoc
+    (into {}
+      (for [x (range 0 4)
+            y (range 0 4)]
+        [[x y] nil]))
+    :score 0))
 
 (defn ^:export refresh []
   (println "Hello"))
